26e39c4b4bebed86623a8f891afe25f75af9ef6e
Srimoyee <30791239+srimoyee1212@users.noreply.github.com>
2024-05-06 12:01:14 -0400

    Update data_preprocessing.py
    fixed data imports

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 
-# Define the path to the directory containing the taxi trip data CSV files
+
 data_dir = "data/yellow_tripdata/"
 
-# Choose a subset of files to load
+# choosing a subset for now
 files_to_load = [
     "yellow_tripdata_2019-01.csv",
     "yellow_tripdata_2019-02.csv",
@@ -12,38 +12,38 @@ files_to_load = [
     "yellow_tripdata_2019-04.csv",
     "yellow_tripdata_2019-05.csv",
     
-    # Add more file names here as needed
+    
 ]
 
-# Read CSV files into pandas DataFrames
+
 dfs = []
 for file_name in files_to_load:
     file_path = os.path.join(data_dir, file_name)
     df = pd.read_csv(file_path)
     dfs.append(df)
 
-# Concatenate DataFrames into a single DataFrame
+
 taxi_data = pd.concat(dfs, ignore_index=True)
 
-# Drop unnecessary columns
-columns_to_drop = ["VendorID", "RateCodeID", "store_and_fwd_flag", "payment_type",
+
+columns_to_drop = ["VendorID", "RatecodeID", "store_and_fwd_flag", "payment_type",
                    "fare_amount", "extra", "mta_tax", "tip_amount", "tolls_amount",
-                   "improvement_surcharge", "total_amount"]
-columns_to_drop = [col for col in columns_to_drop if col in taxi_data.columns]  # Check if column exists
+                   "improvement_surcharge", "total_amount", "congestion_surcharge"]
+#columns_to_drop = [col for col in columns_to_drop if col in taxi_data.columns]  # Check if column exists
 taxi_data.drop(columns=columns_to_drop, inplace=True)
 
 
-# Convert pickup and dropoff datetime columns to datetime objects
+
 taxi_data["tpep_pickup_datetime"] = pd.to_datetime(taxi_data["tpep_pickup_datetime"])
 taxi_data["tpep_dropoff_datetime"] = pd.to_datetime(taxi_data["tpep_dropoff_datetime"])
 
 # Filter out records with invalid or missing data
 taxi_data = taxi_data.dropna()
 
-# Filter out trips with unrealistic distances or durations
-taxi_data = taxi_data[(taxi_data["Trip_distance"] > 0) & (taxi_data["Duration"] > 0)]
+# Filter out trips with unrealistic distances
+taxi_data = taxi_data[(taxi_data["trip_distance"] > 0)]
 
-# Print summary information about the preprocessed data
+
 print("Preprocessed Taxi Trip Data:")
 print(taxi_data.info())
 print(taxi_data.head())
